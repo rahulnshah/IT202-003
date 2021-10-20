7bb4f90f7d66b77652e1156f28ab8171b4c4c3ef
Rahul Shah <rahulnshah3012@gmail.com>
2021-10-19 20:28:37 -0400

    Created FlashMessages branch and implement simplified code to it

--- public_html/Project/logout.php
+++ public_html/Project/logout.php
@@ -2,7 +2,9 @@
 session_start();
 session_unset();
 session_destroy();
-header("Location: login.php");
+session_start();
+//don't require flash.php, this will cause messages
+//to not appear on login and be hidden by the logout transition
 require(__DIR__ . "/../../lib/functions.php");
 flash("Successfully logged out", "success");
 header("Location: login.php");
