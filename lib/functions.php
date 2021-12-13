021c0838b4d1139f3435c6d4b97e20c2f88a8d50
Rahul Shah <rahulnshah3012@gmail.com>
2021-12-13 09:58:34 -0500

    made ratings and reviews of a product visible

--- lib/functions.php
+++ lib/functions.php
@@ -270,4 +270,24 @@ function get_number_of_cartItems()
     }
     return "<pre>" . var_export($e->errorInfo, true) . "</pre>";
 }
+function get_average_rating($id_of_product)
+{
+    $query = "SELECT AVG(rating) AS avgRating FROM Ratings where product_id = :p_id";
+    $db = getDB();
+    $stmt = $db->prepare($query);
+    
+    try{
+        $stmt->execute([":p_id" => $id_of_product]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if(is_null($row["avgRating"]))
+        {
+            return "Be the first to add a rating!";
+        }
+        return number_format($row["avgRating"], 2);
+    }
+    catch (PDOException $e) {
+        flash(var_export($e->errorInfo, true), "warning");
+    }
+    return "<pre>" . var_export($e->errorInfo, true) . "</pre>";
+}
 ?>
--- public_html/Project/product_details.php
+++ public_html/Project/product_details.php
@@ -1,12 +1,7 @@
 <?php
 //note we need to go up 1 more directory
 require(__DIR__ . "/../../partials/nav.php");
-
-// if (!has_role("Admin")) {
-//     flash("You don't have permission to view this page", "warning");
-//     die(header("Location: $BASE_PATH" . "home.php"));
-// }
-//get the table definition
+echo "<pre>" . var_export($_POST,true) . "</pre>";
 $result = [];
 $columns = get_columns("Products");
 //echo "<pre>" . var_export($columns, true) . "</pre>";
@@ -29,7 +24,53 @@ try {
 } catch (PDOException $e) {
     flash("<pre>" . var_export($e, true) . "</pre>");
 }
-//echo "<pre>" . var_export($result,true) . "</pre>";
+if(isset($_POST['comment']) && isset($_POST["vol"]))
+{
+    $comment = se($_POST, "comment", "", false);
+    $rating = (int) se($_POST, "vol", "", false);
+    $haserrors = false;
+    if($rating < 1 || $rating > 5)
+    {
+        flash("invalid rating","warning");
+        $haserrors = true;
+    }
+    if(!preg_match('/^[a-zA-Z0-9 ]*$/', $comment))
+    {
+        flash("Comment must only be alphanumeric and can only contain - or _","warning");
+        $haserrors = true;
+    }
+    if(strlen($comment) <= 0)
+    {
+        flash("Comment must not be blank","warning");
+        $haserrors = true;
+    }
+    if(!$haserrors)
+    {
+            $db = getDB();
+            $stmt = $db->prepare("INSERT INTO Ratings (comment, product_id, user_id, rating) VALUES (:comment, :p_id, :u_id, :rating) ON DUPLICATE KEY UPDATE comment = VALUES(comment), rating = VALUES(rating)"); 
+            try
+            {
+                $stmt->execute([":comment" => $comment,":p_id" => $id, ":u_id" => get_user_id(), ":rating" => $rating]);
+                flash("Thank you for your feedback!");
+            }
+            catch(PDOException $e)
+            {
+                flash(var_export($e->errorInfo, true), "warning");
+            }
+    }
+}
+$allRatings = [];
+$stmt = $db->prepare("SELECT user_id, created, comment, rating FROM Ratings WHERE product_id = :p_id ORDER BY created DESC LIMIT 10");
+try {
+    $stmt->execute([":p_id" => $id]);
+    $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($r) {
+        $allRatings = $r;
+    }
+} catch (PDOException $e) {
+    flash("<pre>" . var_export($e, true) . "</pre>");
+}
+echo "<pre>" . var_export($allRatings, true) . "</pre>";
 ?>
 <div class="container-fluid">
     <h1><?php se($result,"name"); ?> Details</h1>
@@ -48,8 +89,40 @@ try {
         <?php if(has_role("Admin")) : ?>
             <a class="btn btn-primary" href="admin/edit_product.php?id=<?php echo se($result, "id", "", false);?>">Edit</a>
         <?php endif; ?>
+        <!-- add a rating form here with a comment box -->
+        <div class="mb-3">
+            <label for="vol">Rating (between 1 and 5): <?php echo get_average_rating($id)?>/5</label>
+            <br>
+            <input type="range" step="0.01" id="vol" name="vol" min="1" max="5">
+        </div>
+        <div class="mb-3">
+            <label for="d">Comment</label>
+            <textarea class="form-control form-control-sm" name="comment" id="d" placeholder="leave a comment..."></textarea>
+        </div>
+        <!-- submit btn -->
+        <input class="btn btn-primary" type="submit" value="Submit Feedback"/>
     </form>
+    <?php if (count($allRatings) > 0) : ?>
+        <h1>Ratings & Reviews</h1>
+        <hr>
+        <?php foreach ($allRatings as $rating) : ?>
+        <div class="card">
+            <div class="card-body">
+                <p><?php echo " Created: " . se($rating,"created","Unknown created time", false); ?></p>
+                <p><?php se($rating,"comment","Unknown Comment"); ?><p>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </div>
+<script>
+    $(document).ready(function (){
+        const ratingText = document.getElementsByTagName("label")[0].innerText;
+        document.getElementById("vol").oninput = function() { document.getElementsByTagName("label")[0].innerText = ratingText + " " + document.getElementById("vol").value + "/5"};
+    });
+    
+</script>
+
 
 <?php
 //note we need to go up 1 more directory
